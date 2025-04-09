e363029721a94a092795a5d1e3e7298ae3cc8aee
mzeeshantariq <m.zeeshantariq@systemsltd.com>
2025-04-09 16:41:40 +0500

    Put all the optional attributes

--- main.tf
+++ main.tf
@@ -24,16 +24,46 @@ resource "aws_redshift_cluster" "redshift" {
   port               = var.port
 
   # Optional attributes
-  publicly_accessible   = var.publicly_accessible
-  number_of_nodes       = var.number_of_nodes
-  encrypted             = var.encrypted
-  kms_key_id            = var.kms_key_id
-  enhanced_vpc_routing  = var.enhanced_vpc_routing
-  cluster_subnet_group_name = var.cluster_subnet_group_name
-  iam_role_arns         = var.iam_role_arns
-  vpc_security_group_ids = var.vpc_security_group_ids
+  publicly_accessible                 = var.publicly_accessible
+  number_of_nodes                     = var.number_of_nodes
+  encrypted                           = var.encrypted
+  kms_key_id                          = var.kms_key_id
+  enhanced_vpc_routing                = var.enhanced_vpc_routing
+  cluster_subnet_group_name           = var.cluster_subnet_group_name
+  iam_role_arns                       = var.iam_role_arns
+  vpc_security_group_ids              = var.vpc_security_group_ids
   automated_snapshot_retention_period = var.automated_snapshot_retention_period
-  maintenance_window = var.maintenance_window
-  snapshot_schedule = var.snapshot_schedule
-  cluster_parameter_group_name = var.cluster_parameter_group_name
+  maintenance_window                  = var.maintenance_window
+  snapshot_schedule                   = var.snapshot_schedule
+  cluster_parameter_group_name        = var.cluster_parameter_group_name
+
+  default_iam_role_arn                 = var.default_iam_role_arn
+  master_password_wo                   = var.master_password_wo
+  master_password_wo_version           = var.master_password_wo_version
+  master_password_secret_kms_key_id    = var.master_password_secret_kms_key_id
+  multi_az                             = var.multi_az
+  availability_zone                    = var.availability_zone
+  availability_zone_relocation_enabled = var.availability_zone_relocation_enabled
+  preferred_maintenance_window         = var.preferred_maintenance_window
+  cluster_version                      = var.cluster_version
+  allow_version_upgrade                = var.allow_version_upgrade
+  apply_immediately                    = var.apply_immediately
+  elastic_ip                           = var.elastic_ip
+  skip_final_snapshot                  = var.skip_final_snapshot
+  final_snapshot_identifier            = var.final_snapshot_identifier
+  snapshot_arn                         = var.snapshot_arn
+  snapshot_identifier                  = var.snapshot_identifier
+  snapshot_cluster_identifier          = var.snapshot_cluster_identifier
+  owner_account                        = var.owner_account
+  iam_roles                            = var.iam_roles
+  maintenance_track_name               = var.maintenance_track_name
+  manual_snapshot_retention_period     = var.manual_snapshot_retention_period
+  tags                                 = var.tags
+  bucket_name                          = var.bucket_name
+  s3_key_prefix                        = var.s3_key_prefix
+  log_destination_type                 = var.log_destination_type
+  log_exports                          = var.log_exports
+  destination_region                   = var.destination_region
+  retention_period                     = var.retention_period
+  grant_name                           = var.grant_name
 }
--- variables.tf
+++ variables.tf
@@ -1,122 +1,302 @@
 variable "region" {
-    description = "a variable of type string"
-    type = string
-    default = null
+  description = "a variable of type string"
+  type        = string
+  default     = null
 }
 
 # redshift cluster variables
 
 variable "create_redshift_cluster" {
-    description = "a variable of type bool"
-    type        = bool
-    default     = null
+  description = "a variable of type bool"
+  type        = bool
+  default     = null
 }
 
-variable cluster_identifier {
-    description = "a variable of type string"
-    type = string
-    default = null
-} 
-variable database_name {
-    description = "a variable of type string"
-    type = string
-    default = null
+variable "cluster_identifier" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
 }
-variable master_username {
-    description = "a variable of type string"
-    type = string
-    default = null
+variable "database_name" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
 }
-variable master_password {
-    description = "a variable of type string"
-    type = string
-    default = null
+variable "master_username" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
 }
-variable node_type {
-    description = "a variable of type string"
-    type = string
-    default = null
+variable "master_password" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
 }
-variable cluster_type {
-    description = "a variable of type string"
-    type = string
-    default = null
+variable "node_type" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+variable "cluster_type" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
 }
 
 variable "port" {
-    description = "a variable of type number"
-    type = number
-    default = null
+  description = "a variable of type number"
+  type        = number
+  default     = null
 }
 
 variable "publicly_accessible" {
-    description = "a variable of type bool"
-    type = bool
-    default = null
+  description = "a variable of type bool"
+  type        = bool
+  default     = null
 }
 
 variable "number_of_nodes" {
-    description = "a variable of type number"
-    type = number
-    default = null
+  description = "a variable of type number"
+  type        = number
+  default     = null
 }
 
 variable "encrypted" {
-    description = "a variable of type bool"
-    type = bool
-    default = null
+  description = "a variable of type bool"
+  type        = bool
+  default     = null
 }
 
 variable "kms_key_id" {
-    description = "a variable of type string"
-    type = string
-    default = null
+  description = "a variable of type string"
+  type        = string
+  default     = null
 }
 
 variable "enhanced_vpc_routing" {
-    description = "a variable of type bool"
-    type = bool
-    default = null
+  description = "a variable of type bool"
+  type        = bool
+  default     = null
 }
 
 variable "cluster_subnet_group_name" {
-    description = "a variable of type string"
-    type = string
-    default = null
+  description = "a variable of type string"
+  type        = string
+  default     = null
 }
 
 variable "iam_role_arns" {
-    description = "a variable of type list"
-    type = list
-    default = null
+  description = "a variable of type list"
+  type        = list(any)
+  default     = null
 }
 
 variable "vpc_security_group_ids" {
-    description = "a variable of type list"
-    type = list
-    default = null
+  description = "a variable of type list"
+  type        = list(any)
+  default     = null
 }
 
 variable "automated_snapshot_retention_period" {
-    description = "a variable of type number"
-    type = number
-    default = null
+  description = "a variable of type number"
+  type        = number
+  default     = null
 }
 
 variable "maintenance_window" {
-    description = "a variable of type string"
-    type = string
-    default = null
+  description = "a variable of type string"
+  type        = string
+  default     = null
 }
 
 variable "snapshot_schedule" {
-    description = "a variable of type string"
-    type = string
-    default = null
+  description = "a variable of type string"
+  type        = string
+  default     = null
 }
 
 variable "cluster_parameter_group_name" {
-    description = "a variable of type string"
-    type = string
-    default = null
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "default_iam_role_arn" {
+  description = "a variable of type list"
+  type        = list(any)
+  default     = null
+}
+
+variable "master_password_wo" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "master_password_wo_version" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "master_password_secret_kms_key_id" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "multi_az" {
+  description = "a variable of type bool"
+  type        = bool
+  default     = null
+}
+
+variable "vpc_security_group_ids" {
+  description = "a variable of type list"
+  type        = list(any)
+  default     = null
+}
+
+variable "availability_zone" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "availability_zone_relocation_enabled" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "preferred_maintenance_window" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "cluster_version" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "allow_version_upgrade" {
+  description = "a variable of type bool"
+  type        = bool
+  default     = null
+}
+
+variable "apply_immediately" {
+  description = "a variable of type bool"
+  type        = bool
+  default     = null
+}
+
+variable "elastic_ip" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "skip_final_snapshot" {
+  description = "a variable of type bool"
+  type        = bool
+  default     = null
+}
+
+variable "final_snapshot_identifier" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "snapshot_arn" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "snapshot_identifier" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "snapshot_cluster_identifier" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "owner_account" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "iam_roles" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "maintenance_track_name" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "manual_snapshot_retention_period" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  description = "a variable of type map"
+  type        = map(any)
+  default     = null
+}
+
+variable "bucket_name" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "s3_key_prefix" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "log_destination_type" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "log_exports" {
+  description = "a variable of type list"
+  type        = list(any)
+  default     = null
+}
+
+variable "destination_region" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "retention_period" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
+}
+
+variable "grant_name" {
+  description = "a variable of type string"
+  type        = string
+  default     = null
 }
